fdfca975e4c022c3c0c4996839c201d12cf7a885
tal3at7 <tal3at7@gmail.com>
2016-06-16 19:38:52 +0200

    Home

---
+++ home.php
@@ -0,0 +1,117 @@
+<html>
+    <head>
+        <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1 "> 
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+    </head>
+    
+    
+    <body>
+        
+        <!---- Header -->
+        <div class="container-fluid" style="margin-left: -15px">
+            
+            <img  height="1000" src="images/header4.png" >
+            
+        </div>
+            
+        <div class="jumbotron text-center" style="margin-bottom: 0px ;margin-top: -650px;margin-left:-30px;
+  height: 350px;
+  color: white;
+  text-shadow: black 0.3em 0.3em 0.3em;
+  background:transparent;">
+            
+            <h1> <b>Fleet Tracking System</b></h1>
+                <p>Find your bus, Anywhere, Anytime. </p>
+            
+        </div>
+                
+        
+        
+        
+        <div class="row" style="margin-top:200px">
+            <div class="col-sm-8">
+                
+                <div class="jumbotron" style="height:500px; margin-top:150px; padding:70px 0px 0px 200px ; background-color: white">
+            <h1> <b>Fleet Tracking owner</b></h1>
+                <p>bla,kaaabakja ,bla Anywhere, Anytime. </p>
+                 
+                <p>bla,kaaabakja ,bla Anywhere, Anytime. </p> <p>bla,kaaabakja ,bla Anywhere, Anytime. </p> <p>bla,kaaabakja ,bla Anywhere, Anytime. </p>
+                
+                
+                
+                <p style="color:grey ;margin-top:300px">________________________________________________________________________________________________________________________________________ </p>
+            
+            
+            
+        </div>
+                
+            </div>
+            <div class="col-sm-4">
+                <div class="container" style="margin-top:200px">
+  
+  <img src="images/fleetownerrr.png" class="img-circle" alt="Cinque Terre" width="500" height="500">
+</div>
+                
+            </div>
+            
+        </div>
+        
+        
+        
+             
+        <div class="row">
+            
+            
+            
+             <div class="col-sm-4">
+                <div class="container" style="margin-top:200px; margin-left: 70px">
+  
+  <img src="images/passenger.png" class="img-circle" alt="Cinque Terre" width="500" height="500">
+</div>
+                
+            </div>
+            
+            
+            
+            <div class="col-sm-8">
+                
+                <div class="jumbotron " style="height:500px; margin-top:200px; padding:70px 0px 0px 200px ; background-color: white">
+            <h1> <b>passenger</b></h1>
+                <p>bla,kaaabakja ,bla Anywhere, Anytime. </p>
+                 
+                <p>bla,kaaabakja ,bla Anywhere, Anytime. </p> <p>bla,kaaabakja ,bla Anywhere, Anytime. </p> <p>bla,kaaabakja ,bla Anywhere, Anytime. </p>
+                
+                
+                
+            
+        </div>
+                
+            </div>
+           
+            
+        </div>
+        
+        
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+        
+    
+</html>
